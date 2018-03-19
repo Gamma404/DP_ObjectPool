7b27a29f4f8ba6d089e5ccb05fd2f6a00dfdf820
am903103 <am903103@au.edu.tw>
2018-03-19 14:14:10 +0800

    first commit

---
+++ DP_ObjectPool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DP_ObjectPool
+//
+//  Created by Apple on 2018/3/19.
+//  Copyright © 2018年 Gamma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
